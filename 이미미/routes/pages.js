15dcc42d58f68c3c77c68c834ce073357eb447db
mimi123 <alal39603819@gmail.com>
2019-07-23 15:25:31 +0900

    ㅁㅁㅁㅁㅁ

--- 이미미/routes/pages.js
+++ 이미미/routes/pages.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require('../core/user');
 const router = express.Router();
-
+const Board =require('../core/user')
 
 const user = new User();
 
@@ -12,10 +12,10 @@ router.get('/', (req, res, next) => {
         res.redirect('/home');
         return;
     }
-    //시작 주소는 /이고 render로 index라는 화면을 보여주고 사용자가 확인되면 redirect로 /home으로 이동함
+   
     res.render('index', {title:"web"});
     
-})
+})//맨 처음 시작 화면
 
 router.get('/home', (req, res, next) => {//로그인 성공시 홈으로 
     let user = req.session.user;
@@ -26,9 +26,9 @@ router.get('/home', (req, res, next) => {//로그인 성공시 홈으로
     }
     res.redirect('/');
 });
+//로그인 했을 때
 
-
-router.get('/board',(req, res,next)=>{//게시판으로 이동
+router.get('/board',(req, res,next)=>{//게시판 목록으로 이동
     let user = req.session.user;
     
     if(user){
@@ -37,24 +37,23 @@ router.get('/board',(req, res,next)=>{//게시판으로 이동
 
     }
     res.redirect('/board');
-    //res.send('success');
-
-
 });
+//게시판으로 이동했을 때 
+
 
 router.get('/write',(req, res,next)=>{//글쓰기로 이동
     let user = req.session.user;
     
     if(user){
         res.render('write',{title:"write"});
+ 
         return;
-
     }
-    res.redirect('/write');
-   
-
-
+   res.redirect('/board');
+       
 });
+//글 작성화면
+
 
 router.post('/login', (req, res, next) => {//로그인이라는 행동을 함 
     
@@ -72,32 +71,7 @@ router.post('/login', (req, res, next) => {//로그인이라는 행동을 함
     })
 
 });
-
-router.post('/register', (req, res, next) => {//글쓰기
-    
-    let boardInput = {
-        username: req.body.username,
-        title: req.body.title,
-        content: req.body.content,
-        date: req.body.date
-    };//입력한 id, pw
-  
-    user.create(userInput, function(lastId) {//회원가입을 하면
-
-        if(lastId) {
- 
-            user.find(lastId, function(result) {
-                req.session.user = result;
-                req.session.opp = 0;
-                res.redirect('/board');//찾아서 로그인해서 /home으로 감
-            });
-
-        }else {
-            console.log('Error write');
-        }
-    });
-
-});
+//로그인이라는 행동
 
 
 router.post('/register', (req, res, next) => {//가입
@@ -124,8 +98,27 @@ router.post('/register', (req, res, next) => {//가입
     });
 
 });
+//가입하는 행동
 
 
+router.post('/writing', (req, res, next) => {
+    
+    let user = req.session.user;
+
+    let writeInput = {
+        id: req.body.id,
+        title: req.body.title,
+        content: req.body.content,
+        date: req.body.date
+    };
+    console.log(writeInput);
+    
+    user.writing(writeInput, function(result) {
+        
+        res.redirect('/board');
+    });
+    
+});
 
 router.get('/loggout', (req, res, next) => {
     
@@ -134,6 +127,7 @@ router.get('/loggout', (req, res, next) => {
             res.redirect('/');
         });
     }
-});//loggout 버튼 누르면 redirect로 /으로 이동
+});
+//loggout했을 때
 
 module.exports = router;
